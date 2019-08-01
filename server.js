11a1147e6b4738c15493efe80d55ffb0b61182df
Zhehao Li <1003449832@qq.com>
2019-08-01 16:29:52 -0500

    handle POST request

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var http = require('http');
+const http = require('http');
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port = 8080;
-}
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('<h1>Hello World!</h1>'); //write a response to the client
-  res.end(); //end the response
-}).listen(port); //the server object listens on port 8080
+http.createServer((request, response) => {
+  response.write('<h1>Hello World!</h1>');
+  let body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    response.end(body);
+  });
+}).listen(8080);
