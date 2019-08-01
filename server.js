496e61467a6994ebb79354ae06d790f7e813bedf
Zhehao Li <1003449832@qq.com>
2019-08-01 13:20:05 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 8080;
+}
+
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(port); //the server object listens on port 8080
